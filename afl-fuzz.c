googleprojectzero/winafl

commit e0b711f9ca5a85a9a8a4e4108b70e103589a864b

Refactor DRIO tool feature

afl-fuzz.c
@@ -2393,6 +2393,7 @@ static void create_target_process(char** argv) {
   char *pipe_name;
   char *buf;
   char *pidfile;
+  char *client_invocation;
   FILE *fp;
   size_t pidsize;
   BOOL inherit_handles = TRUE;
@@ -2443,6 +2444,11 @@ static void create_target_process(char** argv) {
     inherit_handles = FALSE;
   }
 
+  client_invocation = alloc_printf("-c %s", winafl_dll_path);
+  if (expert_mode) {
+    client_invocation = alloc_printf("-t winafl");
+  }
+
   if(drioless) {
     char *static_config = alloc_printf("%s:%d", fuzzer_id, fuzz_iterations_max);
 
@@ -2453,41 +2459,23 @@ static void create_target_process(char** argv) {
     SetEnvironmentVariable("AFL_STATIC_CONFIG", static_config);
     cmd = alloc_printf("%s", target_cmd);
     ck_free(static_config);
-  } else if (expert_mode) {
-    if (drattach) {
-      drattachpid = find_attach_pid(drattach_identifier);
-      cmd = alloc_printf(
-        "%s\\drrun.exe -attach %ld -t winafl %s -fuzzer_id %s",
-        dynamorio_dir, drattachpid, client_params, fuzzer_id);
-    } else {
-      pidfile = alloc_printf("childpid_%s.txt", fuzzer_id);
-      if (persist_dr_cache) {
-        cmd = alloc_printf(
-          "%s\\drrun.exe -pidfile %s -persist -persist_dir \"%s\\drcache\" -t winafl %s -fuzzer_id %s -drpersist -- %s",
-          dynamorio_dir, pidfile, out_dir, client_params, fuzzer_id, target_cmd);
-      } else {
-        cmd = alloc_printf(
-          "%s\\drrun.exe -pidfile %s -t winafl %s -fuzzer_id %s -- %s",
-          dynamorio_dir, pidfile, client_params, fuzzer_id, target_cmd);
-      }
-    }
   }
   else {
     if (drattach) {
       drattachpid = find_attach_pid(drattach_identifier);
       cmd = alloc_printf(
-        "%s\\drrun.exe -attach %ld -no_follow_children -c winafl.dll %s -fuzzer_id %s",
-        dynamorio_dir, drattachpid, client_params, fuzzer_id);
+        "%s\\drrun.exe -attach %ld -no_follow_children %s %s -fuzzer_id %s",
+        dynamorio_dir, drattachpid, client_invocation, client_params, fuzzer_id);
     } else {
       pidfile = alloc_printf("childpid_%s.txt", fuzzer_id);
       if (persist_dr_cache) {
         cmd = alloc_printf(
-          "%s\\drrun.exe -pidfile %s -no_follow_children -persist -persist_dir \"%s\\drcache\" -c %s %s -fuzzer_id %s -drpersist -- %s",
-          dynamorio_dir, pidfile, out_dir, winafl_dll_path, client_params, fuzzer_id, target_cmd);
+          "%s\\drrun.exe -pidfile %s -no_follow_children -persist -persist_dir \"%s\\drcache\" %s %s -fuzzer_id %s -drpersist -- %s",
+          dynamorio_dir, pidfile, out_dir, client_invocation, client_params, fuzzer_id, target_cmd);
       } else {
         cmd = alloc_printf(
-          "%s\\drrun.exe -pidfile %s -no_follow_children -c %s %s -fuzzer_id %s -- %s",
-          dynamorio_dir, pidfile, winafl_dll_path, client_params, fuzzer_id, target_cmd);
+          "%s\\drrun.exe -pidfile %s -no_follow_children %s %s -fuzzer_id %s -- %s",
+          dynamorio_dir, pidfile, client_invocation, client_params, fuzzer_id, target_cmd);
       }
     }
   }

commit bd3f8408c6ab70aa7f9bf09e0b0688c4e8eb5ccc

Fix memory leak introduced in e0b711f

afl-fuzz.c
@@ -2444,9 +2444,10 @@ static void create_target_process(char** argv) {
     inherit_handles = FALSE;
   }
 
-  client_invocation = alloc_printf("-c %s", winafl_dll_path);
   if (expert_mode) {
     client_invocation = alloc_printf("-t winafl");
+  } else {
+    client_invocation = alloc_printf("-c %s", winafl_dll_path);
   }
 
   if(drioless) {
@@ -2566,6 +2567,7 @@ static void create_target_process(char** argv) {
     child_pid = pi.dwProcessId;
   }
 
+  ck_free(client_invocation);
   ck_free(target_cmd);
   ck_free(cmd);
   ck_free(pipe_name);